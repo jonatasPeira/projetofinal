2de1f4662900fc057e123e7cacbbe29cecdad9ba
frik <lenejonatasfilho@gmail.com>
2024-06-04 16:27:41 +0000

    d

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'firebase_options.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Auth
+import 'package:flutter_firebase/features/app/splash_screen/splash_screen.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/home_page.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/login_page.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/sign_up_page.dart';
 
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
